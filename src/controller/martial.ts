f61df7b7ba9948568dbc7640a9f58901a40a11cc
DaeseongKim <daeseongkim05@gmail.com>
2021-05-19 20:31:48 +0900

    :sparkles: /martial/rank 엔드포인트 구현 1. martial/rank 엔드포인트 구현

--- src/controller/martial.ts
+++ src/controller/martial.ts
@@ -1,9 +1,11 @@
 import { Request, Response } from "express";
 import { truncate } from "fs";
 import Martials from "../model/Martials";
+import Reviews from "../model/Reviews";
 import Users from "../model/Users";
 import Users_Martials from "../model/Users_Martials";
 import { isAuthorized } from "./auth";
+import sequelize, { Op } from "sequelize";
 
 export const info = async (req: Request, res: Response) => {
   try {
@@ -36,9 +38,9 @@ export const bookmark = async (req: Request, res: Response) => {
   }
 };
 export const bookmarkCreate = async (req: Request, res: Response) => {
-  // if (!isAuthorized(req)) {
-  //   res.status(403).send({ message: "Invalid Access Token" });
-  // }
+  if (!isAuthorized(req)) {
+    res.status(403).send({ message: "Invalid Access Token" });
+  }
   try {
     const { Users_id, Martials_id } = req.body;
     const createData = await Users_Martials.create({
@@ -47,14 +49,28 @@ export const bookmarkCreate = async (req: Request, res: Response) => {
     });
     res.status(201).send({ message: "Created" });
   } catch (e) {
-    console.log(e.message);
     res.status(404).json({ message: "Not Found" });
   }
 };
-export const rank = (req: Request, res: Response) => {
+export const rank = async (req: Request, res: Response) => {
   try {
-    res.status(200).send("working...");
+    const averageData = await Reviews.findAll({
+      attributes: [
+        "Martials_id",
+        [sequelize.fn("AVG", sequelize.col("score")), "scoreAvg"],
+      ],
+      group: ["Martials_id"],
+      order: [[sequelize.fn("AVG", sequelize.col("score")), "DESC"]],
+    });
+
+    const ranked = averageData.slice(0, 3).map((el: any) => el.Martials_id);
+    const resultData = await Martials.findAll({
+      where: {
+        [Op.or]: [{ id: ranked[0] }, { id: ranked[1] }, { id: ranked[2] }],
+      },
+    });
+    res.status(200).send({ data: resultData, message: "ok" });
   } catch (e) {
-    console.log(e);
+    res.status(404).json({ message: "Not Found" });
   }
 };
