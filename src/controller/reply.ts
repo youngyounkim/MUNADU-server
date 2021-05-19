b7bce42736a08e0fabb8e4b95274aaa44308e059
rjc1704 <rjc1704@gmail.com>
2021-05-19 16:55:01 +0900

    :sparkles: /reply/create 엔드포인트 구현 1. 작성된 리뷰에 새로운 댓글 추가하는 기능 구현

--- src/controller/reply.ts
+++ src/controller/reply.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import Reviews_Replies from "../model/Reviews_Replies";
 
 export const reviewList = (req: Request, res: Response) => {
   try {
@@ -14,11 +15,26 @@ export const userList = (req: Request, res: Response) => {
     console.log("error...");
   }
 };
-export const create = (req: Request, res: Response) => {
+
+// ? 리뷰에 새로운 댓글을 생성
+export const create = async (req: Request, res: Response) => {
+  // if (!isAuthorized(req)) {
+  //   res.status(403).json({message: "Invalid Access Token"});
+  //   return;
+  // }
   try {
-    res.status(200).send("working...");
-  } catch (e) {
-    console.log("error...");
+    const { comment, userId, reviewId } = req.body;
+    const data = await Reviews_Replies.create({
+      comment: comment,
+      Users_id: userId,
+      Reviews_id: reviewId,
+    });
+    res.status(201).json({ data: data, message: "ok" });
+  } catch (err) {
+    console.log(`err`, err);
+    res.status(404).json({
+      message: "Not Found",
+    });
   }
 };
 export const deleteReply = (req: Request, res: Response) => {
