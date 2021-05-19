f2cc1ce1a54c3d91623b553a56e183bfdb71713c
rjc1704 <rjc1704@gmail.com>
2021-05-19 18:56:28 +0900

    :sparkles: /reply/user-list/:userid 엔드포인트 구현 1. 특정 사용자가 작성한 모든 댓글리스트를 읽어옵니다. 2. 로그인한 사용자만 볼 수 있습니다.

--- src/controller/reply.ts
+++ src/controller/reply.ts
@@ -1,37 +1,103 @@
 import { Request, Response } from "express";
+import Reviews_Replies from "../model/Reviews_Replies";
 
-export const reviewList = (req: Request, res: Response) => {
+// ? 리뷰에 달린 댓글리스트 읽기
+export const reviewList = async (req: Request, res: Response) => {
   try {
-    res.status(200).send("working...");
-  } catch (e) {
-    console.log("error...");
+    const data = await Reviews_Replies.findAll({
+      where: { Reviews_id: req.params.reviewid },
+    });
+    res.status(200).json({ data: data, message: "ok" });
+  } catch (err) {
+    console.log(`err`, err);
+    res.status(404).json({
+      message: "Not Found",
+    });
   }
 };
-export const userList = (req: Request, res: Response) => {
+
+// ? 특정 사용자가 작성한 모든 댓글리스트 읽기
+export const userList = async (req: Request, res: Response) => {
+  // if (!isAuthorized(req)) {
+  //   res.status(403).json({message: "Invalid Access Token"});
+  //   return;
+  // }
   try {
-    res.status(200).send("working...");
-  } catch (e) {
-    console.log("error...");
+    const data = await Reviews_Replies.findAll({
+      where: { Users_id: req.params.userid },
+    });
+    res.status(200).json({ data: data, message: "ok" });
+  } catch (err) {
+    console.log(`err`, err);
+    res.status(404).json({
+      message: "Not Found",
+    });
   }
 };
-export const create = (req: Request, res: Response) => {
+
+// ? 리뷰에 새로운 댓글을 생성
+export const create = async (req: Request, res: Response) => {
+  // if (!isAuthorized(req)) {
+  //   res.status(403).json({message: "Invalid Access Token"});
+  //   return;
+  // }
   try {
-    res.status(200).send("working...");
-  } catch (e) {
-    console.log("error...");
+    const { comment, userId, reviewId } = req.body;
+    const data = await Reviews_Replies.create({
+      comment: comment,
+      Users_id: userId,
+      Reviews_id: reviewId,
+    });
+    res.status(201).json({ data: data, message: "ok" });
+  } catch (err) {
+    console.log(`err`, err);
+    res.status(404).json({
+      message: "Not Found",
+    });
   }
 };
-export const deleteReply = (req: Request, res: Response) => {
+
+// ? 리뷰에 있는 댓글 삭제.
+export const deleteReply = async (req: Request, res: Response) => {
+  // if (!isAuthorized(req)) {
+  //   res.status(403).json({message: "Invalid Access Token"});
+  //   return;
+  // }
   try {
-    res.status(200).send("working...");
-  } catch (e) {
-    console.log(e);
+    const { replyId } = req.body;
+    const data = await Reviews_Replies.destroy({
+      where: { id: replyId },
+    });
+    res.status(200).json({ message: "ok" });
+  } catch (err) {
+    console.log(`err`, err);
+    res.status(404).json({
+      message: "Not Found",
+    });
   }
 };
-export const update = (req: Request, res: Response) => {
+
+// ? 리뷰에 있는 댓글 수정
+export const update = async (req: Request, res: Response) => {
+  // if (!isAuthorized(req)) {
+  //   res.status(403).json({message: "Invalid Access Token"});
+  //   return;
+  // }
   try {
-    res.status(200).send("working...");
-  } catch (e) {
-    console.log(e);
+    const { comment, replyId } = req.body;
+    const data = await Reviews_Replies.update(
+      {
+        comment: comment,
+      },
+      {
+        where: { id: replyId },
+      }
+    );
+    res.status(201).json({ message: "ok" });
+  } catch (err) {
+    console.log(`err`, err);
+    res.status(404).json({
+      message: "Not Found",
+    });
   }
 };
