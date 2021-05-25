f570bee01004a82d3270fe947d7304d744bbc8b7
DaeseongKim <daeseongkim05@gmail.com>
2021-05-25 14:46:20 +0900

    :sparkles: /review/rank 엔드포인트 구현

--- src/router/review.ts
+++ src/router/review.ts
@@ -9,5 +9,6 @@ reviewRouter.post("/create", review.create);
 reviewRouter.delete("/delete", review.deleteReview);
 // "use strict" 모드에서는 delete가 변수 이름으로 사용되면 안된다고 합니다.
 reviewRouter.put("/update", review.update);
+reviewRouter.get("/rank", review.rank);
 
 export default reviewRouter;
